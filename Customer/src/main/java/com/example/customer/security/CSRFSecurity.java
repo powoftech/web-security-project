3af05fa5ce42a3a3a187b3a87d32b45d74f02db7
powoftech <powoftech@gmail.com>
2024-05-06 15:52:37 +0700

    feat: add csrf token to html /search-products form

--- Customer/src/main/java/com/example/customer/security/CSRFSecurity.java
+++ Customer/src/main/java/com/example/customer/security/CSRFSecurity.java
@@ -5,26 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class CSRFSecurity {
     @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
+
+    @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.authorizeRequests(requests -> requests
                 .antMatchers("/cart", "/add-to-cart", "/update-cart", "/check-out", "/add-order", "/customer-profile",
-                        "/update-profile", "/change-password",
-                        "/home", "/", "/login", "/do-login", "/register", "/do-register", "/logout", "/order",
-                        "/cancel-order", "/order-view/**",
-                        "/product", "/product-detail/**", "/search-products", "/search-product/**", "/order-product/**")
+                        "/update-profile", "/change-password", "/home", "/", "/login", "/do-login", "/register",
+                        "/do-register", "/logout", "/order", "/cancel-order", "/order-view/**", "/product",
+                        "/product-detail/**", "/search-products", "/search-products/**", "/order-product/**")
                 .permitAll()
                 .antMatchers("/blog/**", "/css/**", "/fonts/**", "/image/**", "/images/**", "/js/**", "/vendor/**",
-                        "/main.js", "/style.css", "/customJs.js","/customCss.css")
+                        "/main.js", "/style.css", "/customJs.js", "/customCss.css")
                 .permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                .anyRequest().authenticated())
+                .csrf((csrf) -> csrf.csrfTokenRepository(csrfTokenRepository()));
+
         return http.build();
     }
 }
