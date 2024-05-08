cd21421ddef085afa5babe850cb94bf3c3473fbb
NamSellsFish <nhatnam0192123@gmail.com>
2024-05-08 12:32:03 +0700

    Hot Fix Application Error Disclosure and Missing Standardized Error Handling Mechanism

--- Customer/src/main/java/com/example/customer/Controller/MyErrorController.java
+++ Customer/src/main/java/com/example/customer/Controller/MyErrorController.java
@@ -1,25 +1,20 @@
 package com.example.customer.Controller;
-
-
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class MyErrorController implements ErrorController {
-
-    @GetMapping(value = "/error")
-    public String handleError(HttpSession request) {
-
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
-
             int statusCode = Integer.parseInt(status.toString());
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
@@ -31,4 +26,6 @@ public class MyErrorController implements ErrorController {
         }
         return "error";
     }
+
 }
+
